dd4f07f2c759acfa16c590014b96938af4d3e87f
TRrajputDev <tushartanwar183@gmail.com>
2025-01-25 15:57:54 +0530

    PasswordGenerator

---
+++ React/05Passwordgenerator/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className='text-2xl text-zinc-800'>Hey this is a PassWord Generator using tailwind and react</h1>
+    </>
+  )
+}
+
+export default App
