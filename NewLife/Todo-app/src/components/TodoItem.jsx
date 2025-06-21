c49757437114059006533da1e5cd21be7a1da346
TRrajputDev <tushartanwar183@gmail.com>
2025-06-21 11:25:06 +0530

    Added todo completed with react and contextAPI

--- NewLife/Todo-app/src/components/TodoItem.jsx
+++ NewLife/Todo-app/src/components/TodoItem.jsx
@@ -1,11 +1,25 @@
+import React, { useState } from 'react'
+// import { useTodo } from '../contexts/TodoContext';
+import { useTodo } from '../context'
+
 function TodoItem({ todo }) {
-    
+    const [isTodoEditable, setIsTodoEditable] = useState(false)
+    const [todoMsg, setTodoMsg] = useState(todo.todo)
+    const { updateTodo, deleteTodo, toggleComplete } = useTodo()
+
+    const editTodo = () => {
+        updateTodo(todo.id, { ...todo, todo: todoMsg })
+        setIsTodoEditable(false)
+    }
+    const toggleCompleted = () => {
+        //console.log(todo.id);
+        toggleComplete(todo.id)
+    }
 
     return (
         <div
-            className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
-                todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
-            }`}
+            className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
+                }`}
         >
             <input
                 type="checkbox"
@@ -15,9 +29,8 @@ function TodoItem({ todo }) {
             />
             <input
                 type="text"
-                className={`border outline-none w-full bg-transparent rounded-lg ${
-                    isTodoEditable ? "border-black/10 px-2" : "border-transparent"
-                } ${todo.completed ? "line-through" : ""}`}
+                className={`border outline-none w-full bg-transparent rounded-lg ${isTodoEditable ? "border-black/10 px-2" : "border-transparent"
+                    } ${todo.completed ? "line-through" : ""}`}
                 value={todoMsg}
                 onChange={(e) => setTodoMsg(e.target.value)}
                 readOnly={!isTodoEditable}
